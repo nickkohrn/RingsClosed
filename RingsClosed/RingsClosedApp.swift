4f9dde604b8b146e7fb731a88d91ac225e2c61f6
Nick Kohrn <bnkohrn@icloud.com>
2024-05-21 09:32:30 -0400

    Add project files

---
+++ RingsClosed/RingsClosedApp.swift
@@ -0,0 +1,17 @@
+//
+//  RingsClosedApp.swift
+//  RingsClosed
+//
+//  Created by Nick Kohrn on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct RingsClosedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
